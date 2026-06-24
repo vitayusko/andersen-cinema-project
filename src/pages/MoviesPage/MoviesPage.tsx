4ca24d4e9e588c2382a169e78574609667d4e14d
Vita Yusko <vitayusko@gmail.com>
2026-06-24 17:49:34 +0300

    implement Movie Details page and movie filtering
    Currently sorting and filtering are applied to the loaded movies list on the client side. + added pagination

--- src/pages/MoviesPage/MoviesPage.tsx
+++ src/pages/MoviesPage/MoviesPage.tsx
@@ -1,20 +1,51 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import { fetchMovies } from '../../redux/movies/moviesThunks';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { MovieCard } from './MovieCard/MovieCard';
 import styles from './MoviesPage.module.css';
 import { Container } from '../../components/Container/Container';
+import { MoviesControls,  type SortBy } from './MoviesControls/MoviesControls';
 
 export const MoviesPage = () => {
   const dispatch = useAppDispatch();
    const { items: movies, loading, error } = useAppSelector(
     (state) => state.movies,
   );
+   const [searchValue, setSearchValue] = useState('');
+  const [sortBy, setSortBy] = useState<SortBy>('title');
 
-  useEffect(() => {
-    dispatch(fetchMovies(1));
-  }, [dispatch]);
+const [page, setPage] = useState(1);
+
+useEffect(() => {
+  dispatch(fetchMovies(page));
+}, [dispatch, page]);
+
+  // useEffect(() => {
+  //   dispatch(fetchMovies(1));
+  // }, [dispatch]);
+
+  const visibleMovies = useMemo(() => {
+    return [...movies]
+      .filter((movie) =>
+        movie.title.toLowerCase().includes(searchValue.toLowerCase()),
+      )
+      .sort((a, b) => {
+        if (sortBy === 'rating') {
+          return b.vote_average - a.vote_average;
+        }
+
+        if (sortBy === 'year') {
+          return (
+            Number(b.release_date.slice(0, 4)) -
+            Number(a.release_date.slice(0, 4))
+          );
+        }
+
+        return a.title.localeCompare(b.title);
+      });
+  }, [movies, searchValue, sortBy]);
+
    if (loading) {
     return <div>Loading...</div>;
   }
@@ -27,9 +58,13 @@ export const MoviesPage = () => {
   return (
     <Container>
     <div className={styles.container}>
-      <h1>Movies Page</h1>
-      <ul className={styles.list}>
-        {movies.map ((movie) => (
+ <MoviesControls
+          searchValue={searchValue}
+          sortBy={sortBy}
+          onSearchChange={setSearchValue}
+          onSortChange={setSortBy}
+        />      <ul className={styles.list}>
+        {visibleMovies.map ((movie) => (
             <MovieCard key={movie.id} 
             id={movie.id}
             title={movie.title}
@@ -38,6 +73,25 @@ export const MoviesPage = () => {
   voteAverage={movie.vote_average} />
         ))}
       </ul>
+
+      <div className={styles.pagination}>
+  <button
+    type="button"
+    disabled={page === 1}
+    onClick={() => setPage((prev) => prev - 1)}
+  >
+    Prev
+  </button>
+
+  <span>Page {page}</span>
+
+  <button
+    type="button"
+    onClick={() => setPage((prev) => prev + 1)}
+  >
+    Next
+  </button>
+</div>
     </div></Container>
   );
 };
